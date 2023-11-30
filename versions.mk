8bd750e05e1f75d9dc70f188ab7b028bc263add5
Shiva Krishna Merla <smerla@nvidia.com>
2023-11-30 22:46:28 +0000

    Cherry-pick: Merge branch 'fix_unbind_non_gpu_devices' into 'master'

--- versions.mk
+++ versions.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION ?= v0.6.2
+VERSION ?= v0.6.5
 
 vVERSION := v$(VERSION:v%=%)
 
-CUDA_VERSION := 12.2.0
+CUDA_VERSION := 12.3.0
