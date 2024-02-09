0e420a81b6b91adb8575fc060b94f90ac731c51c
Tariq Ibrahim <tibrahim@nvidia.com>
2024-02-08 16:45:53 -0800

    bump cuda base image and golang versions
    Signed-off-by: Tariq Ibrahim <tibrahim@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -17,8 +17,8 @@ MODULE := github.com/NVIDIA/k8s-driver-manager
 
 vVERSION := v$(VERSION:v%=%)
 
-CUDA_VERSION := 12.3.0
-GOLANG_VERSION ?= 1.20.2
+CUDA_VERSION := 12.3.1
+GOLANG_VERSION ?= 1.21.5
 
 BUILDIMAGE_TAG ?= devel-go$(GOLANG_VERSION)
 BUILDIMAGE ?=  ghcr.io/nvidia/k8s-test-infra:$(BUILDIMAGE_TAG)
