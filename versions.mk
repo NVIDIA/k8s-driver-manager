ba2a280532c466767cc9998a34a3922694c543cd
Tariq Ibrahim <tibrahim@nvidia.com>
2025-06-24 09:51:27 -0700

    bump golang version to 1.24.4
    Signed-off-by: Tariq Ibrahim <tibrahim@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -17,7 +17,7 @@ MODULE := github.com/NVIDIA/k8s-driver-manager
 
 vVERSION := v$(VERSION:v%=%)
 
-GOLANG_VERSION ?= 1.23.5
+GOLANG_VERSION ?= 1.24.4
 
-BUILDIMAGE_TAG ?= devel-go$(GOLANG_VERSION)
-BUILDIMAGE ?=  ghcr.io/nvidia/k8s-test-infra:$(BUILDIMAGE_TAG)
+BUILDIMAGE_TAG ?= $(GOLANG_VERSION)-bookworm
+BUILDIMAGE ?=  golang:$(BUILDIMAGE_TAG)
